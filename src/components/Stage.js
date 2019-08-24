a8a0640e8e80414a9ff3353a28e161835f694353
Ben Rout <benadamrout@gmail.com>
2019-08-24 09:49:47 +0100

    Updates Stage component to map the cells from the stage and use styled component

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import { StyledStage } from './styles/StyledStage';
 import Cell from './Cell';
 
 const Stage = ({ stage }) => {
-    <div>
-        <Cell />
-    </div>
+    return (
+        <StyledStage width={stage[0].length} height={stage.length}>
+            {stage.map(row => {
+                return row.map((cell, x) => {
+                    return <Cell key={x} type={cell[0]} />
+                })
+            })}
+        </StyledStage>
+    )
 };
 
 export default Stage;
