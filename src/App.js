2674f683ecda1a32b1d5dc645c151b6a82975031
Ben Rout <benadamrout@gmail.com>
2019-08-24 09:46:30 +0100

    Adds return statement to App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Tetris from './components/Tetris';
 
 const App = () => {
-    <div className="App">
-        <Tetris />
-    </div>
+    return (
+        <div className="App">
+            <Tetris />
+        </div>
+    )
 }
 
 export default App;
