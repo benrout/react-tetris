6b1959c5e11234ea6256fc463fd0011a0502fe83
Ben Rout <benadamrout@gmail.com>
2019-08-24 09:51:11 +0100

    Updates Tetris component to call createStage function and pass to Stage component, import StartButton component and use styled components

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { createStage } from '../gameHelpers';
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
+
 // Components
 import Stage from './Stage';
 import Display from './Display';
@@ -7,13 +10,20 @@ import StartButton from './StartButton';
 
 const Tetris = () => {
     return (
-        <div>
-            <Stage />
-            <aside>
-                <Display text="Score" />
-                <Display text="Rows" />
-                <Display text="Level" />
-            </aside>
-        </div>
+        <StyledTetrisWrapper>
+            <StyledTetris>
+                <Stage stage={createStage()} />
+                <aside>
+                    <div>
+                        <Display text="Score" />
+                        <Display text="Rows" />
+                        <Display text="Level" />
+                    </div>
+                    <StartButton />
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
     )
 }
+
+export default Tetris;
